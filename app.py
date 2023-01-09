1fc1c7b1474fcaa620283d30cc74d1d250d07302
varias070 <varias070@gmail.com>
2023-01-09 22:26:00 +0700

    fix

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from tinydb import TinyDB
-from test_requests import test
+
+from checks import check
+from search import get_template_name
+
 
 app = Flask(__name__)
 db = TinyDB('templates.json')
-test()
+
+
+@app.post("/get_form")
+def handler():
+    params = request.args.to_dict()
+    template = {}
+    for key in params:
+        type_field = check(params[key])
+        template.update({key: type_field})
+    name = get_template_name(template, db)
+    if not name:
+        return template
+    else:
+        return name
